0c47ee112e0ebc9795668d381349339b45b69fc0
eastend-street <ss_1322irf@icloud.com>
2019-09-05 15:27:56 -0700

    edit calculator

---
+++ calculator/index.js
@@ -0,0 +1,26 @@
+let total = 0;
+let input = "";
+let operator = "+";
+let isNum = true;
+
+function enterValue(num) {
+  inNum = true;
+  input += num;
+  document.getElementById("answer").value = input;
+}
+
+function calculate(operatorArg) {
+//   if (isNum == 0) {
+//     isNum = false;
+//     let formula = total + operator + input;
+//     total = eval(formula);
+//     input = "";
+//     document.getElementById("answer").value = total;
+//   }
+//   if (operatorArg == "=") {
+//     total = 0;
+//     operator = "+";
+//   } else {
+//     operator = operatorArg;
+//   }
+}
